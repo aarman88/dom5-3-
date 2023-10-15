56d0531b3dd60514f8f6e00b91e4bced52779c46
Арман <@LAPTOP-8BLD2IN5>
2023-10-15 23:00:29 +0600

    Добавьте файлы проекта.

---
+++ dom5(3)/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            CallMethod();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Исключение в вызывающем методе: {ex.Message}");
+        }
+    }
+
+    static void CallMethod()
+    {
+        try
+        {
+            ThrowException();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Исключение в вызываемом методе: {ex.Message}");
+            throw; // Поднимаем исключение обратно в вызывающий метод
+        }
+    }
+
+    static void ThrowException()
+    {
+        throw new Exception("Генерируем исключение в вызываемом методе.");
+    }
+}
